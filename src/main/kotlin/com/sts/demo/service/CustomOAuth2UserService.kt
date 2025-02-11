f0e4b94449625d9ecce78a2fccc90d13fa741aec
khanhtruong-sgtech <khanh.truong@saigontechnology.com>
2025-02-11 17:38:32 +0700

    feat(KOTLIN-0007): improve oauth2

---
+++ src/main/kotlin/com/sts/demo/service/CustomOAuth2UserService.kt
@@ -0,0 +1,59 @@
+package com.sts.demo.service
+
+import com.sts.demo.entity.User
+import com.sts.demo.enums.Role
+import com.sts.demo.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
+import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CustomOAuth2UserService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) : DefaultOAuth2UserService() {
+
+    override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
+        val oAuth2User = super.loadUser(userRequest)
+        
+        val provider = userRequest.clientRegistration.registrationId
+        val email = when (provider) {
+            "google" -> oAuth2User.attributes["email"] as String
+            "github" -> oAuth2User.attributes["email"] as? String ?:""
+            else -> throw IllegalArgumentException("Unsupported provider: $provider")
+        }
+        
+        val username = when (provider) {
+            "google" -> email.substringBefore("@")
+            "github" -> oAuth2User.attributes["login"] as String
+            else -> throw IllegalArgumentException("Unsupported provider: $provider")
+        }
+
+        var user = userRepository.findByEmail(email)
+        
+        if (user == null) {
+            user = User(
+                username = username,
+                email = email,
+                password = passwordEncoder.encode(UUID.randomUUID().toString()),
+                role = Role.CUSTOMER,
+                oauth2Provider = provider
+            )
+
+                userRepository.save(user)
+                println("Created new OAuth2 user: $email with provider: $provider")
+
+        } else {
+            if (user.oauth2Provider == null) {
+                user.oauth2Provider = provider
+                userRepository.save(user)
+                println("Updated existing user with OAuth2 provider: $provider")
+            }
+        }
+
+        return oAuth2User
+    }
+}
